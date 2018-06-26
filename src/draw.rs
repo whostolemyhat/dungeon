7e621130e6181a0a536ef5018d343f8229f0f787
James Tease <james@jamestease.co.uk>
2018-06-26 17:00:44 +0100

    Use single vec and added corridors

--- src/draw.rs
+++ src/draw.rs
@@ -1,34 +1,62 @@
 extern crate cairo;
 
-use super::{ Room, Level };
+use super::{ Level };
 use std::fs::File;
 use self::cairo::{ Context, Format, ImageSurface };
 
-// const IMAGE_WIDTH: i32 = 768;
-// const IMAGE_HEIGHT: i32 = 640;
-// const SCALE: f64 = 16.0;
+// fn draw_room(context: &Context, room: &Room, scale: f64) {
+//     // context.set_source_rgb(room.colour.r, room.colour.g, room.colour.b);
+//     context.set_source_rgb(1.0, 0.4, 0.2);
+//     context.new_path();
+//     context.move_to(room.x1 as f64 * scale, room.y1 as f64 * scale);
+//     context.line_to(room.x2 as f64 * scale, room.y1 as f64 * scale);
+//     context.line_to(room.x1 as f64 * scale, room.y2 as f64 * scale);
+//     context.move_to(room.x2 as f64 * scale, room.y2 as f64 * scale);
+//     context.line_to(room.x2 as f64 * scale, room.y1 as f64 * scale);
+//     context.line_to(room.x1 as f64 * scale, room.y2 as f64 * scale);
+//     context.close_path();
+//     context.fill();
+// }
 
-fn draw_room(context: &Context, room: &Room, scale: f64) {
-    // context.set_source_rgb(room.colour.r, room.colour.g, room.colour.b);
+fn draw_tile(context: &Context, x: f64, y: f64, x2: f64, y2: f64) {
     context.set_source_rgb(1.0, 0.4, 0.2);
     context.new_path();
-    context.move_to(room.x1 as f64 * scale, room.y1 as f64 * scale);
-    context.line_to(room.x2 as f64 * scale, room.y1 as f64 * scale);
-    context.line_to(room.x1 as f64 * scale, room.y2 as f64 * scale);
-    context.move_to(room.x2 as f64 * scale, room.y2 as f64 * scale);
-    context.line_to(room.x2 as f64 * scale, room.y1 as f64 * scale);
-    context.line_to(room.x1 as f64 * scale, room.y2 as f64 * scale);
+    context.move_to(x as f64, y as f64);
+    context.line_to(x2 as f64, y as f64);
+    context.line_to(x as f64, y2 as f64);
+    context.move_to(x2 as f64, y2 as f64);
+    context.line_to(x2 as f64, y as f64);
+    context.line_to(x as f64, y2 as f64);
     context.close_path();
     context.fill();
+}
+
+fn draw_tiles(context: &Context, board: Vec<i32>, scale: f64) {
+    let width = 48;
+    let mut col = 0;
+    let mut row = 0;
+
+    for tile in board {
+        col = col + 1;
+        if col >= width {
+            col = 0;
+            row = row + 1;
+        }
+
+        if tile == 1 {
+            draw_tile(context, col as f64 * scale, row as f64 * scale, col as f64 * scale + scale, row as f64 * scale + scale);
+        }
+    }
 }
 
 pub fn draw(level: Level, path: &str) -> Result<(), ::std::io::Error> {
     let default_output = format!("{}/{}.png", path, "rooms");
     let surface = ImageSurface::create(Format::ARgb32, level.width * level.tile_size, level.height * level.tile_size).unwrap();
     let ctx = Context::new(&surface);
-    for room in level.rooms {
-        draw_room(&ctx, &room, level.tile_size as f64);
-    }
+    // for room in level.rooms {
+    //     draw_room(&ctx, &room, level.tile_size as f64);
+    // }
+    draw_tiles(&ctx, level.board, level.tile_size as f64);
     let mut file = File::create(default_output)?;
     surface.write_to_png(&mut file).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -14,43 +14,93 @@ pub struct Level {
     tile_size: i32,
     width: i32,
     height: i32,
-    board: Vec<Vec<i32>>,
+    board: Vec<i32>,
     rooms: Vec<Room>,
 }
 
 impl Level {
     fn new(width: i32, height: i32) -> Self {
-        let mut board = Vec::new();
-        for _ in 0..height as usize {
-            let row = vec![0; width as usize];
-            board.push(row);
-        }
-
         Level {
             tile_size: 16,
             width,
             height,
-            board,
+            board: vec![0; (height * width) as usize],
             rooms: Vec::new()
         }
     }
 
+    fn get_tile_coords(&self, x: i32, y: i32) -> usize {
+        (y * self.width + x) as usize
+    }
+
     fn add_room(&mut self, room: &Room) {
-        println!("{:?}", &room);
+        // TODO check bounds
         for row in 0..room.height {
             for col in 0..room.width {
-                self.board[(room.y1 + row) as usize][(room.x1 + col) as usize] = 1;
+                let y = room.y1 + row;
+                let x = room.x1 + col;
+                let coord = self.get_tile_coords(x, y);
+
+                self.board[coord] = 1;
             }
         }
 
         self.rooms.push(*room);
     }
+
+    fn horz_corridor(&mut self, start_x: i32, end_x: i32, y: i32) {
+        for col in start_x..end_x {
+            let pos = self.get_tile_coords(col, y);
+            self.board[pos] = 1;
+        }
+    }
+    fn vert_corridor(&mut self, start_y: i32, end_y: i32, x: i32) {
+        for row in start_y..end_y {
+            let pos = self.get_tile_coords(x, row);
+            self.board[pos] = 1;
+        }
+    }
+
+    fn place_corridors(&mut self, mut rng: StdRng) {
+        // TODO check bounds/len
+        for i in 0..(self.rooms.len() - 1) {
+            let room = self.rooms[i];
+            let other = self.rooms[i + 1];
+
+            // randomly pick vert or horz
+            match Range::new(0, 2).ind_sample(&mut rng) {
+                0 => {
+                    match room.centre.x < other.centre.x {
+                        true => self.horz_corridor(room.centre.x, other.centre.x, room.centre.y),
+                        false => self.horz_corridor(other.centre.x, room.centre.x, room.centre.y)
+                    }
+                    match room.centre.y < other.centre.y {
+                        true => self.vert_corridor(room.centre.y, other.centre.y, other.centre.x),
+                        false => self.vert_corridor(other.centre.y, room.centre.y, other.centre.x)
+                    }
+                }
+                _ => {
+                    match room.centre.y < other.centre.y {
+                        true => self.vert_corridor(room.centre.y, other.centre.y, other.centre.x),
+                        false => self.vert_corridor(other.centre.y, room.centre.y, other.centre.x)
+                    }
+                    match room.centre.x < other.centre.x {
+                        true => self.horz_corridor(room.centre.x, other.centre.x, room.centre.y),
+                        false => self.horz_corridor(other.centre.x, room.centre.x, room.centre.y)
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl fmt::Display for Level {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
-        for row in &self.board {
-            write!(f, "{:?}\n", row)?
+        for (i, row) in self.board.iter().enumerate() {
+            if i > 0 && i % self.width as usize == 0 {
+                write!(f, "\n")?;
+            }
+            write!(f, " {:?} ", row)?;
         }
 
         Ok(())
@@ -83,7 +133,7 @@ impl Room {
             y2: y + height,
             width,
             height,
-            centre: Point{ x: (x + (x + width) / 2), y: (y + (y + height) / 2) }
+            centre: Point{ x: x + (width / 2), y: y + (height / 2) }
         }
     }
 
@@ -102,32 +152,17 @@ fn hash_sum(hash: &str) -> usize {
     hash.as_bytes().into_iter().fold(0, |acc, byte| acc + *byte as usize)
 }
 
-fn main() {
+fn place_rooms(level: &mut Level, mut rng: StdRng) {
     let max_rooms = 10;
-    // TODO export these to draw
-    // let tile_size = 16.0;
-    // let image_width = 768.0;
-    // let image_height = 640.0;
-    // let board_width = image_width / tile_size;
-    // let board_height = image_height / tile_size;
-    let board_width = 48;
-    let board_height = 40;
-    let mut level = Level::new(board_width, board_height);
 
-    let hash = create_seed("brian");
-    let seed: &[_] = &[hash_sum(&hash)];
-    let mut rng: StdRng = SeedableRng::from_seed(seed);
-    // let mut rooms = Vec::new();
-
-    // placement
     let min_room_width = 4;
     let max_room_width = 8;
     let min_room_height = 5;
     let max_room_height = 12;
 
     // TODO fix out of bounds
-    let between = Range::new(0, board_width - max_room_width);
-    let between_y = Range::new(0, board_height - max_room_height);
+    let between = Range::new(0, level.width - max_room_width);
+    let between_y = Range::new(0, level.height - max_room_height);
     let height_range = Range::new(min_room_height, max_room_height);
     let width_range = Range::new(min_room_width, max_room_width);
 
@@ -149,16 +184,24 @@ fn main() {
 
         if !collides {
             level.add_room(&room);
-            // level.rooms.push(room);
         }
     }
+}
 
-    // update board
-    // for room in &rooms {
-    // }
+fn main() {
+    let board_width = 48;
+    let board_height = 40;
+    let mut level = Level::new(board_width, board_height);
+
+    let hash = create_seed("brian");
+    let seed: &[_] = &[hash_sum(&hash)];
+    let rng: StdRng = SeedableRng::from_seed(seed);
+
+    place_rooms(&mut level, rng);
+    level.place_corridors(rng);
 
     println!("{}", level);
-    println!("{:?}", level.rooms);
+    // println!("{:?}", level.rooms);
     draw(level, ".").unwrap();
 }
 
