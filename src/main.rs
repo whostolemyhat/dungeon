c26625183b0a6f40e25c9b16d552685fb6c880bc
James Tease <james@jamestease.co.uk>
2018-06-29 17:12:14 +0100

    use args

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate arrayref;
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+extern crate clap;
 
 pub mod draw;
 mod level;
@@ -13,6 +14,8 @@ mod room;
 
 use sha2::{ Sha256, Digest };
 use rand::prelude::*;
+use rand::distributions::Alphanumeric;
+use clap::{ App, Arg };
 
 use draw::draw;
 use level::Level;
@@ -24,13 +27,52 @@ fn create_hash(text: &str) -> String {
 }
 
 fn main() {
+    // config:
+    // hash (pass hash directly)
+    // text (hashed + used)
+    // width
+    // height
+    // max rooms
+    // room size
+    let matches = App::new("Dungeon")
+                    .version("1.0")
+                    .author("James Baum <@whostolemyhat>")
+                    .arg(Arg::with_name("text")
+                        .short("t")
+                        .long("text")
+                        .takes_value(true)
+                        .help("A string to hash and use as a seed"))
+                    .arg(Arg::with_name("seed")
+                        .short("s")
+                        .long("seed")
+                        .takes_value(true)
+                        .help("An existing seed"))
+                    .get_matches();
+
+    // let text = matches.value_of("text").unwrap_or(thread_rng().sample_iter(&Alphanumeric).take(32).collect::<String>().as_str());
+    // let hash = create_hash(&text);
+    // let hash = match matches.value_of("text") {
+    //     Some(text) => create_hash(&text),
+    //     None => create_hash(&thread_rng().sample_iter(&Alphanumeric).take(32).collect::<String>())
+    // };
+
     let board_width = 48;
     let board_height = 40;
-    let hash = create_hash("ohdearmanuelneuer");
 
-    let mut level = Level::new(board_width, board_height, &hash);
-    let seed = array_ref!(hash.as_bytes(), 0, 32);
-    let mut rng: StdRng = SeedableRng::from_seed(*seed);
+    // let seed = array_ref!(hash.as_bytes(), 0, 32);
+    let seed: String = match matches.value_of("seed") {
+        Some(text) => text.to_string(),
+        None => {
+            match matches.value_of("text") {
+               Some(text) => create_hash(&text),
+               None => create_hash(&thread_rng().sample_iter(&Alphanumeric).take(32).collect::<String>())
+           }
+        }
+    };
+
+    let seed_u8 = array_ref!(seed.as_bytes(), 0, 32);
+    let mut rng: StdRng = SeedableRng::from_seed(*seed_u8);
+    let mut level = Level::new(board_width, board_height, &seed);
 
     level.place_rooms(&mut rng);
     level.place_corridors(&mut rng);
--- src/room.rs
+++ src/room.rs
@@ -24,7 +24,7 @@ impl Room {
             y2: y + height,
             width,
             height,
-            centre: Point{
+            centre: Point {
                 x: x + width / 2 as i32,
                 y: y + height / 2 as i32
             }
@@ -33,5 +33,37 @@ impl Room {
 
     pub fn intersects(&self, other: &Self) -> bool {
         self.x <= other.x2 && self.x2 >= other.x && self.y <= other.y2 && self.y2 >= other.y
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use room::Room;
+
+    #[test]
+    fn test_new_room() {
+        let room = Room::new(2, 12, 8, 9);
+        assert_eq!(room.x, 2);
+        assert_eq!(room.x2, 10);
+        assert_eq!(room.y, 12);
+        assert_eq!(room.y2, 21);
+        assert_eq!(room.width, 8);
+        assert_eq!(room.height, 9);
+        assert_eq!(room.centre.x, 6);
+        assert_eq!(room.centre.y, 16);
+    }
+
+    #[test]
+    fn test_intersects() {
+        let room = Room::new(2, 12, 8, 9);
+        let other = Room::new(3, 12, 8, 9);
+        let third = Room::new(18, 20, 4, 4);
+
+        assert!(room.intersects(&other));
+        assert!(!room.intersects(&third));
+        assert!(other.intersects(&room));
+        assert!(!other.intersects(&third));
+        assert!(!third.intersects(&other));
+        assert!(!third.intersects(&room));
     }
 }
